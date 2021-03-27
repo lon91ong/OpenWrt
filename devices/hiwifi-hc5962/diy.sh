2c3c887401529e328b1df435ddcc72cf77da36aa
lon91ong <lon91ong@gmail.com>
2021-03-27 18:38:22 +0800

    Update diy.sh

--- devices/hiwifi-hc5962/diy.sh
+++ devices/hiwifi-hc5962/diy.sh
@@ -4,18 +4,7 @@ rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
 
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
-
+rm -f ./target/linux/ramips/image/mt7621.mk
 cp -f $GITHUB_WORKSPACE/devices/hiwifi-hc5962/mt7621_hiwifi_hc5962-spi.dts ./target/linux/ramips/dts/
-
-cat >> ./target/linux/ramips/image/mt7621.mk <<EOF
-define Device/hiwifi_hc5962
-  $(Device/dsa-migration)
-  IMAGE_SIZE := 16064k
-  DEVICE_VENDOR := HiWiFi
-  DEVICE_MODEL := HC5962
-  DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2
-endef
-TARGET_DEVICES += hiwifi_hc5962
-EOF
-
+cp -f $GITHUB_WORKSPACE/devices/hiwifi-hc5962/mt7621.mk ./target/linux/ramips/image/
 sed -i 's/^[ \t]*//g' ./target/linux/ramips/image/mt7621.mk
