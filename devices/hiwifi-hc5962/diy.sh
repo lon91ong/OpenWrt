8ab790a7a4696640e9fac7532a0d9770f2e34967
lon91ong <lon91ong@gmail.com>
2021-03-29 19:09:37 +0800

    Update diy.sh

--- devices/hiwifi-hc5962/diy.sh
+++ devices/hiwifi-hc5962/diy.sh
@@ -5,6 +5,7 @@ echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ra
 
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
 rm -f ./target/linux/ramips/image/mt7621.mk
-cp -f $GITHUB_WORKSPACE/devices/hiwifi-hc5962/mt7621_hiwifi_hc5962-spi.dts ./target/linux/ramips/dts/
+rm -f ./target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
+cp -f $GITHUB_WORKSPACE/devices/hiwifi-hc5962/mt7621_hiwifi_hc5962.dts ./target/linux/ramips/dts/
 cp -f $GITHUB_WORKSPACE/devices/hiwifi-hc5962/mt7621.mk ./target/linux/ramips/image/
 sed -i 's/^[ \t]*//g' ./target/linux/ramips/image/mt7621.mk
