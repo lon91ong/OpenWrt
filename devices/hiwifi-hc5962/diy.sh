ab986d74efb99fe519d0ee8b6a288eb54336aa53
lon91ong <lon91ong@gmail.com>
2021-04-01 12:18:05 +0800

    Update diy.sh

--- devices/hiwifi-hc5962/diy.sh
+++ devices/hiwifi-hc5962/diy.sh
@@ -4,6 +4,7 @@ rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
 
 cp -f $GITHUB_WORKSPACE/devices/hiwifi-hc5962/mt7621_hiwifi_hc5962.dts ./target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
+sed -i 's/10.0.0.1/192.168.77.1/g' $GITHUB_WORKSPACE/devices/common/default-settings
 
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
 
