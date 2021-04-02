985b1cf70f853332cd3f757b0ac98d2d45efe7d7
lon91ong <lon91ong@gmail.com>
2021-04-02 18:47:56 +0800

    Update diy.sh

--- devices/hiwifi-hc5962-32m/diy.sh
+++ devices/hiwifi-hc5962-32m/diy.sh
@@ -4,7 +4,7 @@ rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
 
 cp -f $GITHUB_WORKSPACE/devices/hiwifi-hc5962-32m/mt7621_hiwifi_hc5962-32m.dts ./target/linux/ramips/dts/mt7621_hiwifi_hc5962-32m.dts
-sed -i 's/10.0.0.1/192.168.77.1/g' $GITHUB_WORKSPACE/devices/common/default-settings
+sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
 
 cat >> ./target/linux/ramips/image/mt7621.mk <<EOF
 define Device/hiwifi_hc5962-32m
@@ -20,9 +20,3 @@ sed -i 's/^[ \t]*//g' ./target/linux/ramips/image/mt7621.mk
 sed -i 's/hc5962/&|\\\n\thiwifi,hc5962-32m/g' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
-
-sed -i 's/PKG_VERSION:=1/PKG_VERSION:=2/' package/feeds/custom/luci-app-bypass/Makefile
-
-sed -i 's/ +lua-maxminddb//' package/feeds/custom/luci-app-bypass/Makefile
-sed -i '/status_bottom/d' package/feeds/custom/luci-app-bypass/luasrc/model/cbi/bypass/base.lua
-rm -Rf package/feeds/custom/luci-app-bypass/{root/www,root/usr/share/bypass/GeoLite2-Country.mmdb}
