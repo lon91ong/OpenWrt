518e2c4c7ed7baa92d040a3457d8dae9a94a2129
lon91ong <lon91ong@gmail.com>
2021-04-05 10:22:21 +0800

    Update diy.sh

--- devices/hiwifi-hc5962-32m/diy.sh
+++ devices/hiwifi-hc5962-32m/diy.sh
@@ -11,14 +11,12 @@ sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
 cat >> ./target/linux/ramips/image/mt7621.mk <<EOF
 define Device/hiwifi_hc5962-32m
   $(Device/uimage-lzma-loader)
-  IMAGE_SIZE := 32384k
+  IMAGE_SIZE := 32448k
   DEVICE_VENDOR := HiWiFi
   DEVICE_MODEL := HC5962-32M
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 wpad-openssl
 endef
 TARGET_DEVICES += hiwifi_hc5962-32m
 EOF
-sed -i 's/^[ \t]*//g' ./target/linux/ramips/image/mt7621.mk
-sed -i 's/hc5962/&|\\\n\thiwifi,hc5962-32m/g' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
