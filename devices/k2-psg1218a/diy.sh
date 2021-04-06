37c39a5f17438b93a940285ccb3676327116cba1
lon91ong <lon91ong@gmail.com>
2021-04-06 15:06:23 +0800

    覆盖mt7620a_phicomm_psg1218a.dts

--- devices/k2-psg1218a/diy.sh
+++ devices/k2-psg1218a/diy.sh
@@ -7,6 +7,6 @@ svn co https://github.com/x-wrt/x-wrt/trunk/include
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
 rm -rf target/linux/ramips/files/drivers/net/ethernet/ralink
 svn co https://github.com/x-wrt/x-wrt/trunk/target/linux/ramips/files/drivers/net/ethernet/ralink target/linux/ramips/files/drivers/net/ethernet/ralink
-
+mv -f devices/k2-psg1218a/mt7620a_phicomm_psg1218a.dts ./target/linux/ramips/dts/
 sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
 sed -i 's/<0x50000 0x7b0000>/<0x50000 0xfb0000>/g' ./target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
