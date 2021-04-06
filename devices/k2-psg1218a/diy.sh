f7cd0e00335491e7696b25b7bc2ffd805fc3f870
lon91ong <lon91ong@gmail.com>
2021-04-06 14:53:38 +0800

    Update diy.sh

--- devices/k2-psg1218a/diy.sh
+++ devices/k2-psg1218a/diy.sh
@@ -9,3 +9,4 @@ rm -rf target/linux/ramips/files/drivers/net/ethernet/ralink
 svn co https://github.com/x-wrt/x-wrt/trunk/target/linux/ramips/files/drivers/net/ethernet/ralink target/linux/ramips/files/drivers/net/ethernet/ralink
 
 sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
+sed -i 's/<0x50000 0x7b0000>/<0x50000 0xfb0000>/g' ./target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
