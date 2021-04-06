c8c86b7876499d9899a6377352748489af1af7a6
lon91ong <lon91ong@gmail.com>
2021-04-06 20:42:49 +0800

    Update diy.sh

--- devices/hiwifi-hc5962-32m/diy.sh
+++ devices/hiwifi-hc5962-32m/diy.sh
@@ -1,10 +1,3 @@
-find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
-echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
-rm -Rf target/linux/ramips/.svn
-echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
-rm -rf target/linux/ramips/files/drivers/net/ethernet/ralink
-svn co https://github.com/x-wrt/x-wrt/trunk/target/linux/ramips/files/drivers/net/ethernet/ralink target/linux/ramips/files/drivers/net/ethernet/ralink
-
 mv -f devices/hiwifi-hc5962-32m/mt7621_hiwifi_hc5962-32m.dts ./target/linux/ramips/dts/
 sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
 sed -i ':a;N;$!ba;s/hiwifi,hc5962/&|\\\n\t&-32m/1' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
@@ -20,4 +13,4 @@ endef
 TARGET_DEVICES += hiwifi_hc5962-32m
 EOF
 
-sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
+#sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
