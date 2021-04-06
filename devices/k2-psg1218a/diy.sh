8c48b71691bd484c59171c3a6d394a0846976ff0
lon91ong <lon91ong@gmail.com>
2021-04-06 15:07:13 +0800

    覆盖文件替代替换字符操作

--- devices/k2-psg1218a/diy.sh
+++ devices/k2-psg1218a/diy.sh
@@ -9,4 +9,3 @@ rm -rf target/linux/ramips/files/drivers/net/ethernet/ralink
 svn co https://github.com/x-wrt/x-wrt/trunk/target/linux/ramips/files/drivers/net/ethernet/ralink target/linux/ramips/files/drivers/net/ethernet/ralink
 mv -f devices/k2-psg1218a/mt7620a_phicomm_psg1218a.dts ./target/linux/ramips/dts/
 sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
-sed -i 's/<0x50000 0x7b0000>/<0x50000 0xfb0000>/g' ./target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
