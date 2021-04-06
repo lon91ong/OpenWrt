2935a91b02eed669d5294bb7c28f50512e32b871
lon91ong <lon91ong@gmail.com>
2021-04-06 12:01:54 +0800

    bdinfo的02_network配置

--- devices/hiwifi-hc5962-32m/diy.sh
+++ devices/hiwifi-hc5962-32m/diy.sh
@@ -7,7 +7,8 @@ svn co https://github.com/x-wrt/x-wrt/trunk/target/linux/ramips/files/drivers/ne
 
 mv -f devices/hiwifi-hc5962-32m/mt7621_hiwifi_hc5962-32m.dts ./target/linux/ramips/dts/
 sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
-
+sed -i ':a;N;$!ba;s/hiwifi,hc5962/&|\\\n\t&-32m/1' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+sed -i ':a;N;$!ba;s/hiwifi,hc5962)/&|\\\n\thiwifi,hc5962-32m\)/1' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 cat >> ./target/linux/ramips/image/mt7621.mk <<EOF
 define Device/hiwifi_hc5962-32m
   $(Device/uimage-lzma-loader)
