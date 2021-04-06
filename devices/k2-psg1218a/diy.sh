944181de4ba21f39e8f29b5100c77c586b1c161a
lon91ong <lon91ong@gmail.com>
2021-04-06 09:23:35 +0800

    Update diy.sh

--- devices/k2-psg1218a/diy.sh
+++ devices/k2-psg1218a/diy.sh
@@ -5,6 +5,24 @@ svn co https://github.com/x-wrt/x-wrt/trunk/target/linux target/linux
 svn co https://github.com/x-wrt/x-wrt/trunk/include
 
 sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
+rm -rf target/linux/ramips/files/drivers/net/ethernet/ralink
+svn co https://github.com/x-wrt/x-wrt/trunk/target/linux/ramips/files/drivers/net/ethernet/ralink target/linux/ramips/files/drivers/net/ethernet/ralink
+
+mv -f devices/k2-psg1218a/mt7620a_k2_psg1218a.dts ./target/linux/ramips/dts/
+sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
+
+cat >> ./target/linux/ramips/image/mt7620.mk <<EOF
+define Device/k2_psg1218a
+  SOC := mt7620a
+  IMAGE_SIZE := 16064k
+  DEVICE_VENDOR := Phicomm
+  DEVICE_MODEL := PSG1218
+  DEVICE_VARIANT:= Ax
+  DEVICE_PACKAGES := kmod-mt76x2
+  SUPPORTED_DEVICES += psg1218 psg1218a
+endef
+TARGET_DEVICES += k2_psg1218a
+EOF
 
 sed -i 's/O2/Os/g' include/target.mk
 
