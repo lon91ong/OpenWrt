84157a6443ea9030787c0eedab7092c34d81895d
lon91ong <lon91ong@gmail.com>
2021-04-06 20:43:48 +0800

    Update diy.sh

--- devices/k2-psg1218a/diy.sh
+++ devices/k2-psg1218a/diy.sh
@@ -1,3 +1,2 @@
 sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
-sed -i 's/<0x50000 0x7b0000>/<0x50000 0xfb0000>/g' target/linux/ramips/dts/mt7620a_phicomm_psg1218.dtsi
-sed -i 's/<0x50000 0x7b0000>/<0x50000 0xfb0000>/g' target/linux/ramips/dts/mt7620a_phicomm_psg1218a.dts
+sed -i 's/<0x50000 0x7b0000>/<0x50000 0xfb0000>/g' target/linux/ramips/dts/mt7620a_phicomm_psg1218*
