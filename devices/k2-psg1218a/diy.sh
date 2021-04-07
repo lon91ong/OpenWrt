c091779e1db2afbcdf41537a8d5d300468b59a65
lon91ong <lon91ong@gmail.com>
2021-04-07 08:29:12 +0800

    用覆盖dts文件的方式替换字符替换

--- devices/k2-psg1218a/diy.sh
+++ devices/k2-psg1218a/diy.sh
@@ -1,4 +1,5 @@
 rm -rf target/linux/ramips
 svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
 sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
-sed -i 's/<0x50000 0x7b0000>/<0x50000 0xfb0000>/g' target/linux/ramips/dts/mt7620a_phicomm_psg1218*
+#sed -i 's/<0x50000 0x7b0000>/<0x50000 0xfb0000>/g' target/linux/ramips/dts/mt7620a_phicomm_psg1218*
+mv -f devices/k2-psg1218a/mt7620a_phicomm_psg1218a.dts ./target/linux/ramips/dts/
