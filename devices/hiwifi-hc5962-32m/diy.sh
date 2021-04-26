46e2bf5c719d7bf5305b44ce7a1a53e3ba385f32
lon91ong <lon91ong@gmail.com>
2021-04-26 16:33:33 +0800

    Update diy.sh

--- devices/hiwifi-hc5962-32m/diy.sh
+++ devices/hiwifi-hc5962-32m/diy.sh
@@ -1,4 +1,5 @@
 mv -f devices/hiwifi-hc5962-32m/mt7621_hiwifi_hc5962-32m.dts ./target/linux/ramips/dts/
+cp -rf devices/newifi-d2/patches/*.patch devices/hiwifi-hc5962/patches
 sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
 sed -i ':a;N;$!ba;s/hiwifi,hc5962/&|\\\n\t&-32m/1' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 sed -i ':a;N;$!ba;s/hiwifi,hc5962)/hiwifi,hc5962-32m\)/1' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
@@ -13,4 +14,9 @@ endef
 TARGET_DEVICES += hiwifi_hc5962-32m
 EOF
 
-#sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
+find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
+echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
+rm -Rf target/linux/ramips/.svn
+echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
+
+sed -i 's?admin/status/channel_analysis??' package/feeds/luci/luci-mod-status/root/usr/share/luci/menu.d/luci-mod-status.json
