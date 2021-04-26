246ff9b55948ef5202e792939671ada3a2701f7d
lon91ong <lon91ong@gmail.com>
2021-04-26 11:27:56 +0800

    Update diy.sh

--- devices/hiwifi-hc5962/diy.sh
+++ devices/hiwifi-hc5962/diy.sh
@@ -1,4 +1,5 @@
 mv -f devices/hiwifi-hc5962/mt7621_hiwifi_hc5962.dts ./target/linux/ramips/dts/
+cp -rf devices/newifi-d2/patches/*.patch devices/hiwifi-hc5962/patches
 sed -i 's/10.0.0.1/192.168.77.1/g' devices/common/default-settings
 
 sed -i ':a;N;$!ba;s/hiwifi,hc5962)/hiwifi,hc5962-32m\)/1' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
